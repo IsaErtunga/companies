7e88954404ef4b5c942e82a0847dddfa1c26f43a
IsaErtunga <isa.ertunga@gmail.com>
2023-07-02 20:02:21 +0200

    before delete

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,15 +9,73 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region = "eu-north-1"
+variable "AWS_REGION" {
+  default = "eu-north-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0c858d4d1feca5370"
-  instance_type = "t3.micro"
+provider "aws" {
+  region = var.AWS_REGION
+}
 
+# Below Code will generate a secure private key with encoding
+resource "tls_private_key" "key_pair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create the Key Pair
+resource "aws_key_pair" "key_pair" {
+  key_name   = "linux-key-pair"
+  public_key = tls_private_key.key_pair.public_key_openssh
+}
+
+# Save file
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.key_pair.key_name}.pem"
+  content  = tls_private_key.key_pair.private_key_pem
+}
+
+resource "aws_ecr_repository" "companies_repository" {
+  name                 = "ecr-repo-companies"
+  image_tag_mutability = "MUTABLE"
   tags = {
-    Name = "CompaniesAppServerInstance"
+    project = "companies"
+  }
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+}
+
+resource "aws_instance" "companies_server" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.key_pair.key_name
+  user_data     = <<-EOF
+    #!/bin/bash
+    set -ex
+    sudo yum update -y
+    sudo amazon-linux-extras install docker -y
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+  EOF
+  tags = {
+    project = "companies"
+  }
+}
+
+resource "aws_eip" "companies_eip" {
+  instance = aws_instance.companies_server.id
+  vpc      = true
+  tags = {
+    project = "companies"
   }
 }
